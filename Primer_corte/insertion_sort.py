09227dc78060278728da35c6360f9509a4a7c78d
Martinco444 <comartin910@gmail.com>
2025-02-13 12:24:35 -0500

    insertion sort realizado

--- Primer_corte/insertion_sort.py
+++ Primer_corte/insertion_sort.py
@@ -1,12 +1,23 @@
 import numpy as np
 
-nums = np.arange(1,10)
+nums = [5, 7, 2, 4, 1, 6, 3, 10, 8, 9]
 
-def insertion(nums):
+current = 0
+
+for j in range(1, len(nums)):
+    current = nums[j]
+    i = j - 1
+    while i >= 0 and nums[i]>current:
+        nums[i + 1] = nums[i]
+        i = i - 1
+    nums[i + 1] = current
+    
+print(f"el array ordenado es: {nums}")
+        
+        
     
     
     
-    return nums
 
 
 #Leer en que consiste el algoritmo loop invariant, pagina 18 del libro y aplicarlo al de busqueda lineal
